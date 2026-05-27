afd73722b51620345ca86f89c3d511ba350d7d7f
hakabol <habelcijuanu@gmail.com>
2026-05-27 18:25:46 +0530

    \n works and new line happens at the edge of the screen

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,4 @@
-const BUFFER_HEIGHT: usize = 12;
+const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
 #[allow(dead_code)]
@@ -25,7 +25,7 @@ pub enum Color {
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(transparent)]
-struct ColorCode(u8);
+pub struct ColorCode(u8);
 
 impl ColorCode{
     fn new(fg: Color, bg: Color) -> Self {
@@ -48,11 +48,20 @@ struct Buffer {
 
 pub struct Writer {
     column_position: usize,
+    row_position: usize,
     color_code: ColorCode,
     buffer: &'static mut Buffer,
 }
 
 impl Writer{
+    pub fn new(colorcode: ColorCode) -> Self{
+        Writer { 
+            column_position: 0, 
+            row_position: 0, 
+            color_code: colorcode, 
+            buffer: unsafe {&mut *(0xb8000 as *mut Buffer)} 
+        }
+    }
     pub fn write_byte(&mut self, byte: u8){
         match byte{
             b'\n' => self.new_line(),
@@ -62,7 +71,7 @@ impl Writer{
                     self.new_line();
                 }
 
-                let row = BUFFER_HEIGHT-1;
+                let row = self.row_position % BUFFER_HEIGHT;
                 let col = self.column_position;
 
                 let color_code = self.color_code;
@@ -87,12 +96,17 @@ impl Writer{
 
     }
 
-    pub fn new_line(&mut self) {/*TODO*/}
+    pub fn new_line(&mut self) {
+        self.row_position += 1;
+        self.column_position = 0;
+    }
+
 }
 
 pub fn write_smthin(){
     let mut writer = Writer{
         column_position: 34,
+        row_position: 12,
         color_code: ColorCode::new(Color::Red, Color::Blue),
         buffer: unsafe {&mut *(0xb8000 as *mut Buffer)}
     };
